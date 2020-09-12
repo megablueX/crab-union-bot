565b46246ba49430bc5ffec0f9d0eecf6bffe626
megablueX <69505786+megablueX@users.noreply.github.com>
2020-09-12 14:41:54 +1000

    Update groups.js

--- commands/groups.js
+++ commands/groups.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require('discord.js');
-
 module.exports = {
     description: "lists all groups",
     usage: {
@@ -8,7 +7,11 @@ module.exports = {
     checkArgs: (args) => args.length >= 0
 }
 
+module.exports.run = async (client, message, args) => {
+    message.reply("")
 var embed = new MessageEmbed()
+  .setTitle("Here are our groups.")
+  .setDescription("join our groups!");
   .setTitle("Here is a list of our groups.")
   .setDescription("1. https://www.roblox.com/groups/5540783/megabluess-blue-army#!/about");
 message.channel.send(embed);

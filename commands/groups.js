b923ff05db22ac23c44d9aa4262c1a230331fecc
megablueX <69505786+megablueX@users.noreply.github.com>
2020-09-12 15:51:58 +1000

    Update groups.js

--- commands/groups.js
+++ commands/groups.js
@@ -16,3 +16,4 @@ var embed2 = new MessageEmbed()
   .setTitle("Here is a list of our groups.")
   .setDescription("1. https://www.roblox.com/groups/5540783/megabluess-blue-army#!/about");
 message.channel.send(embed2);
+}

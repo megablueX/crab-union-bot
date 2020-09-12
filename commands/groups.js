79316ee9cea23640212ea61eafb7048a4e4de9ac
megablueX <69505786+megablueX@users.noreply.github.com>
2020-09-12 14:34:26 +1000

    Update groups.js

--- commands/groups.js
+++ commands/groups.js
@@ -8,8 +8,6 @@ module.exports = {
     checkArgs: (args) => args.length >= 0
 }
 
-module.exports.run = async (client, message, args) => {
-    message.reply("")
 var embed = new MessageEmbed()
   .setTitle("Here is a list of our groups.")
   .setDescription("1. https://www.roblox.com/groups/5540783/megabluess-blue-army#!/about");

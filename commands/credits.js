965db2f4acecefd17efc421a3bf9c27cfd31e31b
megablueX <69505786+megablueX@users.noreply.github.com>
2020-09-12 11:47:24 +1000

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -9,6 +9,7 @@ module.exports = {
 }
 
 module.exports.run = async (client, message, args) => {
-    message.reply("1. https://www.roblox.com/groups/5540783/megabluess-blue-army#!/about")
+    message.reply("```. https://www.roblox.com/groups/5540783/megabluess-blue-army#!/about"
+```)
 
 }

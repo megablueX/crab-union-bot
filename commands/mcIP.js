191842afb6e0f37444edd6e1ef7d59da87c7068f
No Sound <69505786+megablueX@users.noreply.github.com>
2021-05-09 08:51:18 +1000

    Create mcIP.js

---
+++ commands/mcIP.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    description: "lists all of the people who help make this bot",
+    usage: {
+        "": ""
+    },
+    checkArgs: (args) => args.length >= 0
+}
+
+module.exports.run = async (client, message, args) => {
+    message.reply(" ")
+  var embed = new MessageEmbed() // <-- use this for sending an embedded message
+  .setTitle("This is the mc server IP!")
+  .setDescription("Pyr4midCraftsmp.aternos.me |java edition only|");
+message.channel.send(embed);

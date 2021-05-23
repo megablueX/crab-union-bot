a09e6c7fde6f414525e8840c5dffae443bc8ce3f
No Sound <69505786+megablueX@users.noreply.github.com>
2021-05-23 14:09:21 +1000

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -12,6 +12,6 @@ module.exports.run = async (client, message, args) => {
     message.reply(" ")
   var embed = new MessageEmbed() // <-- use this for sending an embedded message
   .setTitle("This is a list of all the people who helped script bot")
-  .setDescription("SirH scripted bot. No sound is the owner of bot");
+  .setDescription("SirH scripted bot. No sound is the owner and is the person who does most of the updating of bot");
 message.channel.send(embed);
 }
